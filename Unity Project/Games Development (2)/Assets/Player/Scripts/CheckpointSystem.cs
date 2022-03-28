e5d0d3e39bbb660f298c816427b27e08586aae45
FezanTab <79265148+FezanTab@users.noreply.github.com>
2022-03-28 07:04:42 +0100

    Update CheckpointSystem.cs

--- Unity Project/Games Development (2)/Assets/Player/Scripts/CheckpointSystem.cs
+++ Unity Project/Games Development (2)/Assets/Player/Scripts/CheckpointSystem.cs
@@ -5,59 +5,40 @@ using UnityEngine;
 public class CheckpointSystem : MonoBehaviour
 {
 
-   // [SerializeField] public GameObject[] Checkpoints;
-  
+    //variable for storing a spawn location
     public Vector3 spawnPoint;
+    //variable for storing the starting spawn location
     public Vector3 StartingSpawn;
-    //current checkpoint index
-    //int CurrentCPIndex = 0;
+
 
     void Start()
-    {
+    { 
+        //both variables are given the players position vector
         spawnPoint = gameObject.transform.position;
         StartingSpawn = gameObject.transform.position;
-        Debug.Log("Starting spawn");
-        
     }
 
 
-/*  void Update()
-    {
-        
-        //temp death barrier connected to y-axis
-        if (gameObject.transform.position.y < 135f)
-        {
-            gameObject.transform.position = spawnPoint;
-        }
-        
-            }
-*/
-    //checkpoint changer
+
+    //function for colliding with any checkpoint object
     private void OnTriggerEnter(Collider other) 
-    {   
+    {   //if the object is tagged a checkpoint
         if(other.gameObject.CompareTag("Checkpoint"))
         {
+        //set the spawn location by using the position of the object
         spawnPoint = other.transform.position;
-        Debug.Log("SpawnPoint Set");
         }
-
-
-
-
-        /*if(other.gameObject.CompareTag("Checkpoint"))
-        {   
-            spawnPoint = Checkpoints[CurrentCPIndex].transform.position;
-            Destroy(Checkpoints[CurrentCPIndex]);
-            CurrentCPIndex++;
-            Debug.Log("SpawnPoint Set");
-        }
-        */
 
     }
+    
         public void RestartLastCP()
-    {
+    {   
+        //variable saetup for UI restart from last checkpoint button in the pause Ui
         GameObject.Find("Player_Scene1").transform.position = GameObject.Find("Player_Scene1").GetComponent<CheckpointSystem>().spawnPoint;
+        //gives the players full health and 5 charges in the gunsabre
         GameObject.Find("Player_Scene1").GetComponent<PlayerController>().health = 100;
+        GameObject.Find("Player_Scene1").GetComponent<PlayerController>().chargeBar = 5;
+        
     }
 
 
