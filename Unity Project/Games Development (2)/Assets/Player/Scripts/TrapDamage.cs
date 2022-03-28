88cc31b32d52adf0f14138e319f6d31aeaff21d3
FezanTab <79265148+FezanTab@users.noreply.github.com>
2022-03-28 07:19:40 +0100

    Update TrapDamage.cs

--- Unity Project/Games Development (2)/Assets/Player/Scripts/TrapDamage.cs
+++ Unity Project/Games Development (2)/Assets/Player/Scripts/TrapDamage.cs
@@ -6,17 +6,17 @@ public class TrapDamage : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-
+        //Checks if player is colliding with tag
         if(other.gameObject.CompareTag("Trap Injury") ) 
         {
+            //player losing 50 health
             GameObject.Find("Player_Scene1").GetComponent<PlayerController>().health = 50;
-            Debug.Log("Injury working");
         }
 
         if(other.gameObject.CompareTag("InstaKill") ) 
-        {
+        {   
+            //kills the player
             GameObject.Find("Player_Scene1").GetComponent<PlayerController>().health = 00;
-            Debug.Log("InstaKill working");
         }
 
     }
